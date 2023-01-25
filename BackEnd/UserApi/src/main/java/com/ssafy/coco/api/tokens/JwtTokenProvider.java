c1d721fd32ed7f3aa6c21e39553d6dc2f2bcd59c
강원경 <kokk012@naver.com>
2023-01-25 17:18:27 +0900

    Token 유효시간 조정 -> 서비스의 특성을 반영하여 기존 시간에서 대폭 증가시킴.

--- BackEnd/UserApi/src/main/java/com/ssafy/coco/api/tokens/JwtTokenProvider.java
+++ BackEnd/UserApi/src/main/java/com/ssafy/coco/api/tokens/JwtTokenProvider.java
@@ -48,8 +48,8 @@ public class JwtTokenProvider {
 	@Value("${jwt.secret}")
 	private String uniqueKey;
 
-	private int accessTokenValidTime = 1000 * 60 * 30; // AccessToken 유효시간 : 30분
-	private int refreshTokenValidTime = 1000 * 60 * 60; // RefreshToken 유효시간 : 60분
+	private int accessTokenValidTime = 1000 * 60 * 90; // AccessToken 유효시간 : 90분
+	private int refreshTokenValidTime = 1000 * 60 * 60 * 12; // RefreshToken 유효시간 : 12시간
 
 	private final UserDetailsService userDetailsService;
 	private final MemberRepository memberRepository;
