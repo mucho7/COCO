1aefc7b4ae91e566c42a2fa3bed1a4bf367df27e
yadon <mucho7@gmail.com>
2023-01-26 16:46:16 +0900

    0126 last commit

--- front/src/components/signin/SigninForm.js
+++ front/src/components/signin/SigninForm.js
@@ -35,11 +35,6 @@ function SigninForm() {
         }
     }, [inputID])
 
-    // 실시간 ID validation
-    useEffect(() => {
-        setIsOkToSubmit(idValidation()) 
-    }, [inputID, idValidation])
-
     const passwordValidation = useCallback(() => {
         const passwordForm = /^[a-z0-9]{4,12}$/
         const passwordErrorMessage = {
@@ -61,10 +56,6 @@ function SigninForm() {
             setIsPasswordValid({isValid: false})
         }
     }, [inputPassword, inputCheckPassword])
-
-    useEffect(() => {
-        setIsOkToSubmit(passwordValidation())
-    }, [inputPassword, inputCheckPassword, passwordValidation])
 
     // case를 이용한 typing
     const onTypingHandler = (e) => {
@@ -112,6 +103,8 @@ function SigninForm() {
     // 제출
     const onClickHandler = () => {
         setIsEmailValid(!(emailValidation.test(inputEmail)))
+        passwordValidation()
+        idValidation()
         if (isOkToSubmit){axios_test()} else { alert('다시!!')}
     }
 
