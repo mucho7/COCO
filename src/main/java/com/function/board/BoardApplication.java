eabd4dcb0d56fd3cdc7d707b062ee7cb84a82491
mmy789 <mmy789@naver.com>
2023-01-31 10:30:23 +0900

    Fix: netflix-eureka 버전 명시

--- src/main/java/com/function/board/BoardApplication.java
+++ src/main/java/com/function/board/BoardApplication.java
@@ -2,10 +2,11 @@ package com.function.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-// @EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class BoardApplication {
 	public static void main(String[] args) {
