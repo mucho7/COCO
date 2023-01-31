7436c2808dfe3cd4cef506545e39a8ca39bb8048
mmy789 <mmy789@naver.com>
2023-01-31 10:06:07 +0900

    백업

---
+++ src/main/java/com/function/board/service/BoardService.java
@@ -0,0 +1,71 @@
+package com.function.board.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.function.board.domain.board.Board;
+import com.function.board.domain.board.BoardRepository;
+import com.function.board.dto.board.BoardListResponseDto;
+import com.function.board.dto.board.BoardResponseDto;
+import com.function.board.dto.board.BoardSaveRequestDto;
+import com.function.board.dto.board.BoardUpdateRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+	private final BoardRepository boardRepository;
+
+	@Transactional
+	public Long save(BoardSaveRequestDto requestDto) {
+		return boardRepository.save(requestDto.toEntity()).getId();
+	}
+
+	@Transactional
+	public Long update(Long boardId, BoardUpdateRequestDto requestDto) {
+		Board board = boardRepository.findById(boardId)
+			.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+
+		board.update(requestDto.getTitle(), requestDto.getContent(), requestDto.getCode());
+		return boardId;
+	}
+
+	public BoardResponseDto findById(Long boardId) {
+		Board entity = boardRepository.findById(boardId)
+			.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+		return new BoardResponseDto(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public List<BoardListResponseDto> findAll() {
+		return boardRepository.findAll().stream()
+			.map(BoardListResponseDto::new)
+			.collect(Collectors.toList());
+	}
+
+	@Transactional
+	public void delete(Long boardId) {
+		var board = boardRepository.findById(boardId)
+			.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
+		boardRepository.delete(board);
+	}
+
+	@Transactional
+	public int updateView(Long boardId) {
+		return boardRepository.updateView(boardId);
+	}
+
+	public Page<Board> paging(int page, int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		Page<Board> boardList = boardRepository.findAll(pageable);
+		return boardList;
+	}
+}
