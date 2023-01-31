886bec30ff0123e7ec44108eb16d9e85771715cd
서울_15반_이명규 <si44440@naver.com>
2023-01-31 09:08:14 +0900

    m

--- src/main/java/com/function/board/BoardApplication.java
+++ src/main/java/com/function/board/BoardApplication.java
@@ -2,10 +2,10 @@ package com.function.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableJpaAuditing
 @SpringBootApplication
+@EnableEurekaClient
 public class BoardApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/function/board/controller/TestController.java
+++ src/main/java/com/function/board/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/board")
-public class TestController {
+public class testcontroller {
 
 	@GetMapping("/hello")
 	public String hello(){
