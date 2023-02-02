0ed9cb45192a68da6073a65bb74ba96147e791e5
강원경 <kokk012@naver.com>
2023-02-02 12:01:56 +0900

    Style 파라미터이름 수정
    loadUserByUsername
    - 직관적으로 이해하기 쉬운 코드를 위해 파라미터 이름을 username에서 userId로 변경.

--- src/main/java/com/service/gateway/member/CustomUserDetailService.java
+++ src/main/java/com/service/gateway/member/CustomUserDetailService.java
@@ -1,4 +1,4 @@
-package com.service.gateway.memberinfo;
+package com.service.gateway.member;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,10 +14,9 @@ public class CustomUserDetailService implements UserDetailsService {
 	private final MemberRepository memberRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		System.out.println("loadUserByUsername의 파라미터: " + username);
-		return memberRepository.findByUserId(username)
+	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+		System.out.println("[loadUserByUsername@CustomUserDetailService] userId: " + userId);
+		return memberRepository.findByUserId(userId)
 			.orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-
 	}
 }
