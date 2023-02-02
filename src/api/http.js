2094610a2c0cc57db7ccc22eb3f4531dcf3ef779
yadon <mucho7@gmail.com>
2023-02-02 15:59:01 +0900

    Refactor: user detail

--- src/api/http.js
+++ src/api/http.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default axios.create({
 // env로 대체할 것
 // docker측에 push할땐 변경할 것
-  baseURL: "localhost:8000/member",
+  baseURL: "http://localhost:8000/member",
 
 // develope할때 사용할 
 // baseURL: "http://i8a703.p.ssafy.io:8012/member",
