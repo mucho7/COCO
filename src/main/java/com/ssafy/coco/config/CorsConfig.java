cde3ee8d2986df09e95bffaa76e12efdc94d2fdf
강원경 <kokk012@naver.com>
2023-02-03 13:21:55 +0900

    Fix CorsConfig 설정 삭제
    Cors 설정
    - Gateway 단계에서 설정을 하므로 중복된 설정 오류 발생.

--- src/main/java/com/ssafy/coco/config/CorsConfig.java
+++ src/main/java/com/ssafy/coco/config/CorsConfig.java
@@ -1,37 +1,28 @@
 package com.ssafy.coco.config;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-	@Bean
-	public CorsFilter corsFilter() {
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-
-		config.setAllowCredentials(true);
-		// config.addAllowedOrigin("*");
-		config.addAllowedOriginPattern("*"); // addAllowedOriginPattern("*") 대신 사용
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		config.setExposedHeaders(Collections.singletonList("*"));
-		config.addExposedHeader("*");
-		source.registerCorsConfiguration("/**", config);
-
-		return new CorsFilter(source);
-	}
+	// @Bean
+	// public CorsFilter corsFilter() {
+	//
+	// 	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	// 	CorsConfiguration config = new CorsConfiguration();
+	//
+	// 	config.setAllowCredentials(true);
+	// 	// config.addAllowedOrigin("*");
+	// 	config.addAllowedOriginPattern("*"); // addAllowedOriginPattern("*") 대신 사용
+	// 	config.addAllowedHeader("*");
+	// 	config.addAllowedMethod("*");
+	// 	config.setExposedHeaders(Collections.singletonList("*"));
+	// 	config.addExposedHeader("*");
+	// 	source.registerCorsConfiguration("/**", config);
+	//
+	// 	return new CorsFilter(source);
+	// }
 
 }
 
