8b9ad6ef64733b80114682e24fc6e84fe43af294
mmy789 <mmy789@naver.com>
2023-02-06 10:03:57 +0900

    Refactor: @CrossOrigin 추가

--- src/main/java/com/function/board/BoardApplication.java
+++ src/main/java/com/function/board/BoardApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableJpaAuditing
 @EnableEurekaClient
+@CrossOrigin("*")
 @SpringBootApplication
 public class BoardApplication {
 	public static void main(String[] args) {
