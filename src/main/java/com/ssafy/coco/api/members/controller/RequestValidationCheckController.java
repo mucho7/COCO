25415c321b48e404cc87860bbef08d91a52e772f
강원경 <kokk012@naver.com>
2023-02-06 13:09:49 +0900

    Feat 요청 검증 기능 추가
    validateRequest
    - 사용자가 브라우저의 쿠키 값을 임의로 변경하여 위조된 사용자 ID로 요청을 보냈는지 여부를 검증할 수 있도록 메소드 추가
    
    RequestValidationCheckController
    - 검증을 요청할 쿠키 값과 Jwt 토큰 정보가 담긴 요청 헤더

---
+++ src/main/java/com/ssafy/coco/api/members/controller/RequestValidationCheckController.java
@@ -0,0 +1,33 @@
+package com.ssafy.coco.api.members.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.coco.api.tokens.service.JwtTokenService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+
+@Api(tags = "중복 검사 API")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/validate")
+public class RequestValidationCheckController {
+
+	private final JwtTokenService jwtTokenService;
+
+	@PostMapping
+	@ApiOperation(value = "요청 검증", notes = "넘겨받은 회원정보를 바탕으로 회원을 DB에 등록한다.")
+	public boolean validateRequest(@RequestBody @ApiParam(value = "브라우저에 있는 사용자 ID", required = true) String userId,
+		HttpServletRequest request) {
+		String accessToken = request.getHeader("Authorization");
+		return jwtTokenService.validateRequest(userId, accessToken);
+	}
+
+}
