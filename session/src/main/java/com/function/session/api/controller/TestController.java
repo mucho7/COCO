f077857d3ae6fa3e7b84ce46aa181055b4215edf
leejiyeon <sky75013@naver.com>
2023-02-06 09:15:32 +0900

    testController

--- session/src/main/java/com/function/session/api/controller/TestController.java
+++ session/src/main/java/com/function/session/api/controller/TestController.java
@@ -1,2 +1,14 @@
-package com.function.session.api.controller;public class TestController {
+package com.function.session.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
+public class TestController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "session";
+	}
 }
