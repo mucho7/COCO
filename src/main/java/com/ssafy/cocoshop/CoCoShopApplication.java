0b3aacf1a20ea6afacd324b34bcb0991b9a5c6c4
강원경 <kokk012@naver.com>
2023-02-06 10:27:15 +0900

    Feature 프로젝트 생성
    Initial Commit

---
+++ src/main/java/com/ssafy/cocoshop/CoCoShopApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.cocoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoCoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/cocoshop/CoCoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.cocoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoCoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
