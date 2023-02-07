310c0216014d566c9dbc314ba51ca2873bd36e39
강원경 <kokk012@naver.com>
2023-02-07 13:35:00 +0900

    Style : 평판점수 변경 메소드 수정
    updatePointByUsername
    - Transactional → 향후 구매 실패 관련 Transaction을 한번에 처리하기 위해 Transactional 삭제
    - 평판점수 변경 로직 → amount 값을 기존 점수에 더했을때 음수가 되는지 여부로 잔액이 부족한지 여부 및 overflow가 발생하는지 여부를 한번에 확인할 수 있도록 로직 수정.

--- src/main/java/com/ssafy/cocoshop/utils/members/service/MemberService.java
+++ src/main/java/com/ssafy/cocoshop/utils/members/service/MemberService.java
@@ -1,7 +1,5 @@
 package com.ssafy.cocoshop.utils.members.service;
 
-import javax.transaction.Transactional;
-
 import org.springframework.stereotype.Service;
 
 import com.ssafy.cocoshop.utils.members.data.Member;
@@ -20,17 +18,16 @@ public class MemberService {
 			.orElseThrow(() -> new IllegalArgumentException("[" + userId + "] 사용자를 찾을 수 없습니다."));
 	}
 
-	@Transactional
 	public void updatePointByUsername(String userId, int amount) {
 		Member member = memberRepository.findByUserId(userId)
 			.orElseThrow(() -> new IllegalArgumentException("[" + userId + "] 사용자를 찾을 수 없습니다."));
 		if (member.getDelFlag() != null) {// error code: 500
 			throw new IllegalArgumentException("[" + userId + "] 사용자는 탈퇴한 사용자입니다.");
 		} else {
-			if (amount>member.getRating())
+			if (member.getRating() + amount < 0)
 				throw new ArithmeticException("평판점수는 음수가 될 수 없습니다.");
 			else {
-				member.setRating(member.getRating() - amount);
+				member.setRating(member.getRating() + amount);
 			}
 		}
 	}
