df1aec2ce6a064ff03fce911637a7387c9409d58
강원경 <kokk012@naver.com>
2023-02-09 10:04:49 +0900

    Refactor : Token 검증 순서 수정
    AuthenticationHeaderFilter
    - 발생할 수 있는 상황을 세분화하여 각각의 상황에 맞는 로그를 기록하거나 핸들러를 추가하여 그 상황을 처리할 수 있도록 수정
    - 표준 입출력이 아닌 로거를 통해 기록을 남기도록 수정

--- src/main/java/com/service/gateway/tokens/AuthenticationHeaderFilter.java
+++ src/main/java/com/service/gateway/tokens/AuthenticationHeaderFilter.java
@@ -14,8 +14,10 @@ import com.service.gateway.tokens.dto.JwtTokenDto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 public class AuthenticationHeaderFilter extends AbstractGatewayFilterFactory<AuthenticationHeaderFilter.Config> {
 
@@ -37,46 +39,39 @@ public class AuthenticationHeaderFilter extends AbstractGatewayFilterFactory<Aut
 			// Step 1. RequestHeader에서 jwt 토큰 추출
 			JwtTokenDto tokenDto = jwtTokenProvider.resolveToken(
 				request); // 유효하지 않은 세션 (refreshToken이 DB에 있는 값과 다른 값)인지 판단
-			// 유효한 로그인 세션이라면
-			if (tokenDto != null) {
+
+			// Step 2. 토큰의 유효성 검사
+			if (tokenDto != null) { // 유효한 로그인 세션이라면
 				String accessToken = tokenDto.getAccessToken();
 				String refreshToken = tokenDto.getRefreshToken();
 
-				// Step 2. 토큰의 유효성 검사
-				if (!jwtTokenProvider.validateToken(accessToken) && jwtTokenProvider.validateToken(refreshToken)) {
-					if (accessToken != null) {
-						System.out.println(
-							"만료된 AccessToken : " + accessToken + "\n헤더에서 refreshToken을 발견하여 재발급 처리를 진행합니다.");
+				if (jwtTokenProvider.validateToken(refreshToken)) { // 만약 refreshToken의 유효시간이 아직 남았다면
+					if (!jwtTokenProvider.validateToken(accessToken)) { // 만약 AccessToken의 유효시간이 만료되었다면
+						log.info("유효한 로그인 세션이나, AccessToken이 만료되었습니다. AccessToken을 재발급합니다.");
 						// 재발급 후, 컨텍스트에 다시 넣기
-						// Refresh Token 검증
-						boolean validateRefreshToken = jwtTokenProvider.validateToken(refreshToken);
-						// Refresh Token 저장소 존재유무 확인
-						boolean isRefreshToken = jwtTokenProvider.existsRefreshToken(refreshToken);
-						if (validateRefreshToken && isRefreshToken) {
-							// Refresh Token으로 사용자 ID 가져오기
-							String userId = jwtTokenProvider.getUserIdFromRefreshToken(refreshToken);
-							System.out.println("재발급 과정에서 refreshtoken을 기반으로 DB에 등록된 사용자 ID: " + userId);
-							// UserId로 권한정보 받아오기
-							List<String> roles = jwtTokenProvider.getRoles(userId);
-							// 토큰 발급
-							JwtTokenDto newJwtToken = jwtTokenProvider.createToken(userId, roles);
-							System.out.println(
-								"[doFilterInternal@AuthenticationHeaderFilter] newJwtToken:\n" + newJwtToken);
-							// 헤더에 토큰 정보(AccessToken, refreshToken) 추가
-							response.getHeaders().add("Authorization", "bearer " + newJwtToken.getAccessToken());
-							response.getHeaders().add("refreshToken", "bearer " + newJwtToken.getRefreshToken());
-							System.out.println("response header에 넣기 성공");
+						// Refresh Token으로 사용자 ID 가져오기
+						String userId = jwtTokenProvider.getUserIdFromRefreshToken(refreshToken);
+						// UserId로 권한정보 받아오기
+						List<String> roles = jwtTokenProvider.getRoles(userId);
+						// 토큰 발급
+						JwtTokenDto newJwtToken = jwtTokenProvider.createToken(userId, roles);
+						// 헤더에 토큰 정보(AccessToken, refreshToken) 추가
+						response.getHeaders().add("Authorization", "bearer " + newJwtToken.getAccessToken());
+						response.getHeaders().add("refreshToken", "bearer " + newJwtToken.getRefreshToken());
 
-							ServerHttpRequest newRequest = exchange.getRequest()
-								.mutate()
-								.header("Authorization", new String[] {"bearer " + newJwtToken.getAccessToken()})
-								.header("refreshToken", new String[] {"bearer " + newJwtToken.getRefreshToken()})
-								.build();
+						ServerHttpRequest newRequest = exchange.getRequest()
+							.mutate()
+							.header("Authorization", "bearer " + newJwtToken.getAccessToken())
+							.header("refreshToken", "bearer " + newJwtToken.getRefreshToken())
+							.build();
 
-							newExchange = exchange.mutate().request(newRequest).build();
-							System.out.println("request header에 넣기 성공");
-						}
+						newExchange = exchange.mutate().request(newRequest).build();
+					} else {
+						log.info("AccessToken의 유효시간이 아직 남아있습니다.");
 					}
+				} else {
+					log.info("로그인 세션 유효시간이 만료되었습니다. 백엔드 접근 요청을 거부합니다.");
+					return handleUnAuthorized(exchange);
 				}
 				return chain.filter(newExchange);
 			} else {
