db61545ccef028c65b298603b7967b24e8373363
leejiyeon <sky75013@naver.com>
2023-02-10 10:18:50 +0900

    kurento client uri 변경

--- src/main/java/com/function/session/SessionApplication.java
+++ src/main/java/com/function/session/SessionApplication.java
@@ -33,8 +33,8 @@ public class SessionApplication implements WebSocketConfigurer {
 
 	@Bean
 	public KurentoClient kurentoClient() {
-		return KurentoClient.create();
-		// return KurentoClient.create("ws://i8a703.p.ssafy.io:8888/kurento");
+		// return KurentoClient.create();
+		return KurentoClient.create("ws://i8a703.p.ssafy.io:8888/kurento");
 	}
 
 	@Bean
