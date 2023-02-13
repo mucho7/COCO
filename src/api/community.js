257764f15f60fe76ae64eaf3718144412f3c64c5
jhyeop <pizzauniv99@gmail.com>
2023-02-13 15:40:05 +0900

    Fix: change api baseURLs

--- src/api/community.js
+++ src/api/community.js
@@ -2,12 +2,7 @@ import axios from "axios";
 
 // axios 객체 생성
 const api = axios.create({
-  // env로 대체할 것
-  // baseURL: "http://APIgateway:8000/",
-  baseURL: "http://i8a703.p.ssafy.io:8000",
-  // 
-  // baseURL: "http://i8a703.p.ssafy.io:8011",
-
+  baseURL: "https://ssafy.cossafyco.kro.kr/api/",
   headers: {
     "Content-Type": "application/json",
   },
