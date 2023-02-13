420038b913da03855b595c5f809f3c75313e5b28
강원경 <kokk012@naver.com>
2023-02-13 15:28:38 +0900

    Style : 불필요한 코드 삭제

--- src/main/java/com/ssafy/upload/UploadServiceApplication.java
+++ src/main/java/com/ssafy/upload/UploadServiceApplication.java
@@ -2,9 +2,8 @@ package com.ssafy.upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 
-@SpringBootApplication(exclude = { MongoDataAutoConfiguration.class})
+@SpringBootApplication
 public class UploadServiceApplication {
 
 	public static void main(String[] args) {
