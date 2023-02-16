58247fc4ca40893d0db16b312aa4ba075e749e8f
yadon <mucho7@gmail.com>
2023-02-16 12:39:09 +0900

    Fix: Carousel 완전삭제

--- src/components/home/HomeCarousel.js
+++ src/components/home/HomeCarousel.js
@@ -9,40 +9,40 @@ import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
 
 function HomeCarousel() {
     
-    const items = [
-        { title: 'Item 1', image: FirstImg },
-        { title: 'Item 2', image: SecondImg },
-        { title: 'Item 3', image: ThirdImg },
-    ];
+    // const items = [
+    //     { title: 'Item 1', image: FirstImg },
+    //     { title: 'Item 2', image: SecondImg },
+    //     { title: 'Item 3', image: ThirdImg },
+    // ];
     
-    const [currentItem, setCurrentItem] = useState(0);
+    // const [currentItem, setCurrentItem] = useState(0);
 
-    const handleNext = () => {
-        setCurrentItem(currentItem === items.length - 1 ? 0 : currentItem + 1);
-      };
+    // const handleNext = () => {
+    //     setCurrentItem(currentItem === items.length - 1 ? 0 : currentItem + 1);
+    //   };
       
-    const handlePrevious = () => {
-    setCurrentItem(currentItem === 0 ? items.length - 1 : currentItem - 1);
-    };
+    // const handlePrevious = () => {
+    // setCurrentItem(currentItem === 0 ? items.length - 1 : currentItem - 1);
+    // };
 
-    return (
-        <Box sx={{ position: '', width: "100%" }}>
-          <img src={items[currentItem].image} width="100%" alt={items[currentItem].title} />
-          <IconButton
-            sx={{ position: 'absolute', left: 0 }}
-            onClick={handlePrevious}
-          >
-            <KeyboardArrowLeft />
-          </IconButton>
-          <IconButton
-            sx={{ position: 'absolute', right: 0 }}
-            onClick={handleNext}
-          >
-            <KeyboardArrowRight />
-          </IconButton>
-      </Box>
-      
-    )
+    // return (
+    //     <Box sx={{ position: '', width: "100%" }}>
+    //       <img src={items[currentItem].image} width="100%" alt={items[currentItem].title} />
+    //       <IconButton
+    //         sx={{ position: 'absolute', left: 0 }}
+    //         onClick={handlePrevious}
+    //       >
+    //         <KeyboardArrowLeft />
+    //       </IconButton>
+    //       <IconButton
+    //         sx={{ position: 'absolute', right: 0 }}
+    //         onClick={handleNext}
+    //       >
+    //         <KeyboardArrowRight />
+    //       </IconButton>
+    //   </Box>
+    // 
+    // )
 }
 
 export default HomeCarousel;
