565ea564e40bba74a0afd73f82a1b0b3900d49ee
서울_15반_이명규 <si44440@naver.com>
2023-02-22 21:31:20 +0900

    Fix : socket 추가 후 signaling 서버로 활용

--- src/components/Session/IdeArea/Ide.js
+++ src/components/Session/IdeArea/Ide.js
@@ -32,7 +32,8 @@ function Ide(props) {
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
     docRef.current = new Y.Doc();
-    providerRef.current = new WebrtcProvider(`monaco-${roomId}`, docRef.current);
+    const socket = new WebSocket(`wss://ssafy.cossafyco.kro.kr:8443/groupcall`);
+    providerRef.current = new WebrtcProvider(`monaco-${roomId}`, docRef.current,{ signaling: [socket] });
     const yText = docRef.current.getText("monaco");
 
     bindingRef.current = new MonacoBinding(
