083abf6c8394a84334a6357108789609bad742bf
서울_15반_이명규 <si44440@naver.com>
2023-02-23 09:08:13 +0900

    fix : check new url

--- src/components/Session/IdeArea/Ide.js
+++ src/components/Session/IdeArea/Ide.js
@@ -32,7 +32,7 @@ function Ide(props) {
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
     docRef.current = new Y.Doc();
-    providerRef.current = new WebrtcProvider(`monaco-${roomId}`, docRef.current);
+    providerRef.current = new WebrtcProvider(`monaco-${roomId}`, docRef.current, { signaling: ['wss://ssafy.cossafyco.kro.kr:8443/groupcall'] });
     const yText = docRef.current.getText("monaco");
 
     bindingRef.current = new MonacoBinding(
